04bb61d69a954a11a53c306ca20cdf3dcf520950
DavidDev25 <d.werner@david-werner.devgit config --global user.email>
2024-11-05 19:00:50 +0100

    creating new class named moveable-object-class.js

---
+++ classes/moveable-object.class.js
@@ -0,0 +1,9 @@
+class moveableObject{
+    x = 120; 
+    y = 400;    
+    img; 
+
+    moveRight(){
+    console.log('Moving right');
+    }
+} 
--- js/game.js
+++ js/game.js
@@ -1,13 +1,12 @@
 let canvas;
 let ctx;
-let character = new Image();
-
-
-
+let character = new moveableObject();
+ 
 function init(){
-    canvas = document.getElementById('canvas')
+    canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    character.src = ('../img/2_character_pepe/2_walk/W-21.png')
+    console.log('My Character is', character);
+    
 
 }
