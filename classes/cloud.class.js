14916f5e2dfb3e2cd53301e77f3d7a36c640b327
DavidDev25 <d.werner@david-werner.devgit config --global user.email>
2024-11-07 23:07:38 +0100

    added movement Animation on pepe, added chicken Movement to left.

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -3,17 +3,16 @@ class Cloud extends MoveableObject{
     height = 250;
     width = 550;
 
+
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png')
-
         this.x = Math.random() * 500;
         this.animate();
     }
 
     animate(){
-        setInterval(() => {
-            this.x -= 0.11;    
-        }, 1000 / 60);
-        
+        this.moveLeft();
     }
+
+
 }
--- classes/moveable-objecs.class.js
+++ classes/moveable-objecs.class.js
@@ -4,13 +4,27 @@ class MoveableObject{
     height = 150;
     width = 100;
     img;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.11;
 
     loadImage(path){
         this.img = new Image();
         this.img.src = path
     }
 
-    moveLeft(){        
+    loadImages(arr){
+        arr.forEach((path) => {
+        let img = new Image()
+        img.src = path;
+        this.imageCache[path] =  img;
+        });
+    }
+
+    moveLeft(){
+        setInterval(() => {
+            this.x -= this.speed;    
+        }, 1000 / 60); 
     }
 
     moveRight(){
