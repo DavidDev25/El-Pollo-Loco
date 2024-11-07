68cda662457a93d7cb90cbee6766de79d7d0e3b3
DavidDev25 <d.werner@david-werner.devgit config --global user.email>
2024-11-07 18:50:24 +0100

    adjust cloud hight and added animation to clouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,11 +1,19 @@
 class Cloud extends MoveableObject{
     y = 20;
     height = 250;
-    width = 300;
-    
+    width = 550;
+
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png')
 
-        this.x = Math.random() * 500;  
+        this.x = Math.random() * 500;  // Zahl zwischen 200 und 700
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.x -= 0.11;    
+        }, 1000 / 60);
+        
     }
 }
